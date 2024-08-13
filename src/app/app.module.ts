e76f58353bc53d55834d28f156ba6e23df610fbb
Pritish Bhatnagar <pritishbhatnagar7b@gmail.com>
2024-08-13 16:37:46 +0530

    ad sense script tag 3rd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { AdsenseModule } from 'ng2-adsense';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdsenseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
